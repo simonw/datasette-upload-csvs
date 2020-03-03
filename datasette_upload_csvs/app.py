91e2a946848899476eecb499877fdf2a54d9b0fb
Simon Willison <swillison@gmail.com>
2020-03-02 20:02:06 -0800

    Process uploaded CSV in a thread, with a progress bar (#7)
    * WIP progress bars for CSV processing, refs #6 and #4
    
    * Initial work on progress bar and drag/drop
    
    * Test for progress bar table
    
    * Write progress record to DB earlier
    
    * Implemented server-side processing progress bar
    
    * Use correct database path for Ajax polling
    
    * Depend on Datasette 0.37.1

--- datasette_upload_csvs/app.py
+++ datasette_upload_csvs/app.py
@@ -3,7 +3,11 @@ from starlette.endpoints import HTTPEndpoint
 from urllib.parse import quote_plus
 import csv as csv_std
 import codecs
+import datetime
+import io
+import os
 import sqlite_utils
+import uuid
 
 
 class UploadApp(HTTPEndpoint):
@@ -26,26 +30,81 @@ class UploadApp(HTTPEndpoint):
     async def post(self, request):
         formdata = await request.form()
         csv = formdata["csv"]
-        # csv.file is a SpooledTemporaryFile, I can read it directly
-        # NOTE: this is blocking - a better implementation would run this
-        # in a thread.
+        # csv.file is a SpooledTemporaryFile. csv.filename is the filename
         filename = csv.filename
-        # TODO: Support other encodings:
-        reader = csv_std.reader(codecs.iterdecode(csv.file, "utf-8"))
-        headers = next(reader)
-        docs = (dict(zip(headers, row)) for row in reader)
         if filename.endswith(".csv"):
             filename = filename[:-4]
 
         # Import data into a table of that name using sqlite-utils
         db = self.get_database()
 
-        def fn(conn):
+        total_size = get_temporary_file_size(csv.file)
+        task_id = str(uuid.uuid4())
+
+        def insert_initial_record(conn):
             database = sqlite_utils.Database(conn)
-            database[filename].insert_all(docs, alter=True)
+            database["_csv_progress_"].insert(
+                {
+                    "id": task_id,
+                    "filename": filename,
+                    "bytes_todo": total_size,
+                    "bytes_done": 0,
+                    "rows_done": 0,
+                    "started": str(datetime.datetime.utcnow()),
+                    "completed": None,
+                },
+                pk="id",
+                alter=True,
+            )
+
+        await db.execute_write_fn(insert_initial_record, block=True)
+
+        def insert_docs(conn):
+            database = sqlite_utils.Database(conn)
+            # TODO: Support other encodings:
+            reader = csv_std.reader(codecs.iterdecode(csv.file, "utf-8"))
+            headers = next(reader)
+
+            docs = (dict(zip(headers, row)) for row in reader)
+
+            i = 0
+
+            def docs_with_progress():
+                nonlocal i
+                for doc in docs:
+                    i += 1
+                    yield doc
+                    if i % 10 == 0:
+                        database["_csv_progress_"].update(
+                            task_id, {"rows_done": i, "bytes_done": csv.file.tell(),}
+                        )
+
+            database[filename].insert_all(
+                docs_with_progress(), alter=True, batch_size=100
+            )
+            database["_csv_progress_"].update(
+                task_id,
+                {
+                    "rows_done": i,
+                    "bytes_done": total_size,
+                    "completed": str(datetime.datetime.utcnow()),
+                },
+            )
             return database[filename].count
 
-        num_docs = await db.execute_write_fn(fn, block=True)
+        await db.execute_write_fn(insert_docs)
+
+        if formdata.get("xhr"):
+            return JSONResponse(
+                {
+                    "url": "/{database}/{table}".format(
+                        database=quote_plus(db.name), table=quote_plus(filename),
+                    ),
+                    "database_path": quote_plus(db.name),
+                    "task_id": task_id,
+                    "bytes_todo": total_size,
+                }
+            )
 
         if formdata.get("xhr"):
             return JSONResponse(
@@ -59,11 +118,15 @@ class UploadApp(HTTPEndpoint):
 
         return HTMLResponse(
             await self.datasette.render_template(
-                "upload_csv_done.html",
-                {
-                    "database": self.get_database().name,
-                    "table": filename,
-                    "num_docs": num_docs,
-                },
+                "upload_csv_done.html", {"database": db.name, "table": filename,},
             )
         )
+
+
+def get_temporary_file_size(file):
+    if isinstance(file._file, (io.BytesIO, io.StringIO)):
+        return len(file._file.getvalue())
+    try:
+        return os.fstat(file._file.fileno()).st_size
+    except Exception:
+        raise
--- tests/test_datasette_upload_csvs.py
+++ tests/test_datasette_upload_csvs.py
@@ -1,4 +1,6 @@
 from datasette.app import Datasette
+import asyncio
+import json
 import pytest
 import httpx
 import sqlite_utils
@@ -14,7 +16,12 @@ async def test_upload(tmpdir):
     datasette = Datasette([path])
 
     # First test the upload page exists
-    async with httpx.AsyncClient(app=datasette.app()) as client:
+
+    dispatch = httpx.ASGIDispatch(
+        app=datasette.app(), client=("1.2.3.4", 123), raise_app_exceptions=True
+    )
+
+    async with httpx.AsyncClient(dispatch=dispatch) as client:
         response = await client.get("http://localhost/-/upload-csv")
         assert 200 == response.status_code
         assert b'<form action="/-/upload-csv" method="post"' in response.content
@@ -22,8 +29,21 @@ async def test_upload(tmpdir):
         # Now try uploading a file
         files = {"csv": ("dogs.csv", "name,age\nCleo,5\nPancakes,4", "text/csv")}
         response = await client.post("http://localhost/-/upload-csv", files=files)
-        assert b"<h1>Upload complete</h1>" in response.content
-        assert b"Imported 2 rows into" in response.content
+        assert b"<h1>Upload in progress</h1>" in response.content
+
+        # Now things get tricky... the upload is running in a thread, so poll for completion
+        await asyncio.sleep(1)
+        response = await client.get(
+            "http://localhost/data/_csv_progress_.json?_shape=array"
+        )
+        rows = json.loads(response.content)
+        assert 1 == len(rows)
+        assert {
+            "filename": "dogs",
+            "bytes_todo": 26,
+            "bytes_done": 26,
+            "rows_done": 2,
+        }.items() <= rows[0].items()
 
     dogs = list(db["dogs"].rows)
     assert [{"name": "Cleo", "age": "5"}, {"name": "Pancakes", "age": "4"}] == dogs
