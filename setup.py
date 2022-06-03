7f2bacb1b3029df2d33cd49f645d9c33e74dbfa6
Simon Willison <swillison@gmail.com>
2022-06-03 08:28:57 -0700

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     version=VERSION,
     packages=["datasette_upload_csvs"],
     entry_points={"datasette": ["upload_csvs = datasette_upload_csvs"]},
+    python_requires=">=3.7",
     install_requires=[
         "datasette>=0.61",
         "asgi-csrf>=0.7",
